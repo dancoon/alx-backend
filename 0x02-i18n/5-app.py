0be96f8c9c640f8a7e760f53e5e3c889a389ead7
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-11-03 22:04:01 +0300

    Parametrize templates

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -3,7 +3,7 @@
 Get locale from request
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -21,6 +21,25 @@ class Config:
 
 
 app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(login_as):
+    """returns a user dictionary or None if the ID cannot be found or if
+    login_as was not passed"""
+    return users.get(int(login_as), None) if login_as else None
+
+
+@app.before_request
+def before_request():
+    """find a user if any, and set it as a global on flask.g.user"""
+    g.user = get_user(request.args.get("login_as"))
 
 
 @babel.localeselector
@@ -41,7 +60,7 @@ def index():
     """
     render 3-index.html
     """
-    return render_template("4-index.html")
+    return render_template("5-index.html")
 
 
 if __name__ == "__main__":
